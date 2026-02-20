079f00079f27770881f78264d410722a455fd568
Kaif <kaifattari0123@gmail.com>
2026-02-20 12:02:53 +0500

    tasbeeh parlo

---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+function Counter (){
+     const [count, countbraho] = useState(0)
+    return(<div>
+      <h2>Tasbeeh Counter</h2>
+      
+      <h5>count</h5>
+      
+    </div>)
+   
+}
+export default Counter
